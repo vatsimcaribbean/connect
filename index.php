6e4e4de000e84f990b65b2ecfb9ba228a4e93708
Alex Long <33168652+FlynZSky@users.noreply.github.com>
2022-11-17 16:54:46 -0500

    Initial

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+ // Required for VATSIM Connect
+ $clientID                   = $_ENV['CLIENT_ID'];          // VATSIM Connect Client ID
+ $clientSecret               = $_ENV['CLIENT_SECRET'];      // VATSIM Connect Client Secret
+ $scopes                     = $_ENV['SCOPES'];             // VATSIM Connect Scopes
+ $redirectURL                = $_ENV['REDIRECT_URL'];       // VATSIM Connect Redirect URL
+
+ // Required for Plugin
+ $loginDeniedURL             = $_ENV['LOGIN_DENIED'];       // LOCAL Redirect URL Login Denied
+ $logoutURL                  = $_ENV['LOGOUT'];             // LOCAL Redirect URL Logout Message
+
+ $urlAuthorize               = 'https://auth-dev.vatsim.net/oauth/authorize';        // VATSIM Authorize URL
+ $urlAccessToken             = 'https://auth-dev.vatsim.net/oauth/token';            // VATSIM Token URL
+ $urlResourceOwnerDetails    = 'https://auth-dev.vatsim.net/api/user';               // VATSIM User Details URL
+
+$provider = new \League\OAuth2\Client\Provider\GenericProvider([
+    'clientId'                => $clientID,
+    'clientSecret'            => $clientSecret,
+    'redirectUri'             => $redirectURL,
+    'urlAuthorize'            => $urlAuthorize,
+    'urlAccessToken'          => $urlAccessToken,
+    'urlResourceOwnerDetails' => $urlResourceOwnerDetails,
+    'scopes'                  => $scopes
+]);
+
+// If we don't have an authorization code then get one
+if (!isset($_GET['code'])) {
+
+    // Fetch the authorization URL from the provider; this returns the
+    // urlAuthorize option and generates and applies any necessary parameters
+    // (e.g. state).
+    $authorizationUrl = $provider->getAuthorizationUrl();
+
+    // Get the state generated for you and store it to the session.
+    $_SESSION['oauth2state'] = $provider->getState();
+
+    // Optional, only required when PKCE is enabled.
+    // Get the PKCE code generated for you and store it to the session.
+    //$_SESSION['oauth2pkceCode'] = $provider->getPkceCode();
+
+    // Redirect the user to the authorization URL.
+    header('Location: ' . $authorizationUrl);
+    exit;
+
+// Check given state against previously stored one to mitigate CSRF attack
+} elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+
+    if (isset($_SESSION['oauth2state'])) {
+        unset($_SESSION['oauth2state']);
+    }
+
+    exit('Invalid state');
+
+} else {
+
+    try {
+    
+        // Optional, only required when PKCE is enabled.
+        // Restore the PKCE code stored in the session.
+        //$provider->setPkceCode($_SESSION['oauth2pkceCode']);
+
+        // Try to get an access token using the authorization code grant.
+        $accessToken = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        // We have an access token, which we may use in authenticated
+        // requests against the service provider's API.
+
+        // echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+        // echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
+        // echo 'Expired in: ' . $accessToken->getExpires() . "<br>";
+        // echo 'Already expired? ' . ($accessToken->hasExpired() ? 'expired' : 'not expired') . "<br>";
+
+        // Using the access token, we may look up details about the
+        // resource owner.
+        $resourceOwner = $provider->getResourceOwner($accessToken);
+
+         // Fetch VATSIM data from resource owner
+         $userData               = $resourceOwner->toArray();
+         $vatsimEmail            = $userData['data']['personal']['email'];               // VATSIM Registered Email
+         $vatsimCID              = $userData['data']['cid'];                             // VATSIM CID
+         $vatsimNameFirst        = $userData['data']['personal']['name_first'];          // VATSIM Registered First Name
+         $vatsimNameLast         = $userData['data']['personal']['name_last'];           // VATSIM Registered Last Name
+         $vatsimCountryCode      = $userData['data']['personal']['country']['id'];       // VATSIM Registered Country Code
+         $vatsimCountryName      = $userData['data']['personal']['country']['name'];     // VATSIM Registered Country Name
+         $vatsimRatingID         = $userData['data']['vatsim']['rating']['id'];          // VATSIM ATC Rating ID
+         $vatsimRatingShort      = $userData['data']['vatsim']['rating']['short'];       // VATSIM ATC Rating Short
+         $vatsimRatingLong       = $userData['data']['vatsim']['rating']['long'];        // VATSIM ATC Rating Long
+         $vatsimRegionName       = $userData['data']['vatsim']['region']['name'];        // VATSIM Region Name
+         $vatsimRegionID         = $userData['data']['vatsim']['region']['id'];          // VATSIM Region ID
+         $vatsimDivisionName     = $userData['data']['vatsim']['division']['name'];      // VATSIM Division Name
+         $vatsimDivisionID       = $userData['data']['vatsim']['division']['id'];        // VATSIM Division ID
+         $vatsimSubDivisionName  = $userData['data']['vatsim']['subdivision']['name'];   // VATSIM SubDivision Name
+         $vatsimSubDivisionID    = $userData['data']['vatsim']['subdivision']['id'];     // VATSIM SubDivision ID
+         $vatsimToken            = $userData['data']['oauth']['token_valid'];            // VATSIM Oauth Token Valid Boolean
+
+        // var_export($resourceOwner->toArray());
+         
+        // Place your code here to start your session and use VATSIM data from above
+
+
+         // 
+
+
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+        // Failed to get the access token or user details.
+        exit($e->getMessage());
+
+    }
+
+}
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,5 @@
+# VATSIM Connect Standard PHP OAuth 2 Authentication
+
+### Installation
+- rename .env file & set env variable data
+- composer install
